4d9ec29713f0c94ff0a6ecbcc60624aa8f94d4fd
eko muliyo <ekomuliyo@gmail.com>
2020-04-07 10:00:56 +0700

    chaining promise

--- fundamental-frontend-web-development/promise/index.js
+++ fundamental-frontend-web-development/promise/index.js
@@ -13,8 +13,6 @@ const executorFunction = (resolve, reject) => {
 const makeCoffe = new Promise(executorFunction);
 console.log(makeCoffe);
 
-
-console.log();
 // onFulfilled and onRejected
 const executorFunction2 = (resolve, reject) =>{
     const isCoffeMakerReady = false;
@@ -59,3 +57,63 @@ const makeCoffe3 = new Promise(executorFunction3);
 makeCoffe3
     .then(handlerSuccess2) // method then ini dipanggil ketika kondisi promise terpenuhi (fulfilled)
     .catch(handlerRejected2)  // method catch ini dipanggil ketika kondisi promise gagal / atau tidak terpeuhni (rejected)
+
+// Chaining Promise
+const state = {
+    isCoffeMakerReady: true,
+    seedStocks: {
+        arabica: 250,
+        robusta: 60,
+        liberica: 80
+    }
+}
+
+const getSeeds = (type, miligrams) => {
+    return new Promise((resolve, rejected) => {
+        if(state.seedStocks[type] >= miligrams){
+            state.seedStocks[type] -= miligrams;
+            resolve("Biji kopi didapatkan atau tersedia");
+        }else{
+            rejected("maaf stock habis");
+        }
+    });
+}
+
+const makeCoffe4 = seeds => {
+    return new Promise((resolve, rejected) => {
+        if(state.isCoffeMakerReady){
+            resolve(`kopi berhasil dibuat ${seeds}`);
+        }else{
+            rejected("maaf mesin kopi tidak dapat digunakan");
+        }
+    });
+}
+
+const servingToTable = coffe => {
+    return new Promise(resolve => {
+        resolve(`pesanan Pesanan kopi ${coffe} selesai`);
+    })
+}
+
+function reserveCoffe(type, miligrams){
+    getSeeds(type, miligrams)
+        .then(makeCoffe4(type)
+            .then(resolveValueCoffe => {
+                console.log(resolveValueCoffe)
+            })
+        )
+        .then(servingToTable(type)
+            .then(resolveValueServing => {
+                console.log(resolveValueServing)
+            })    
+        )
+        .then(resolveValue => {
+            console.log(resolveValue);
+        })
+        .catch(rejectedReason => {
+            console.log(rejectedReason);
+        });
+}
+
+console.log();
+reserveCoffe("liberica", 80);
