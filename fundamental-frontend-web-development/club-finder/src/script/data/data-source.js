5b9cf809c7a96d02f3f5621856235273d63b0674
eko muliyo <ekomuliyo@gmail.com>
2020-04-09 20:19:29 +0700

    practical promise

--- fundamental-frontend-web-development/club-finder/src/script/data/data-source.js
+++ fundamental-frontend-web-development/club-finder/src/script/data/data-source.js
@@ -1,18 +1,14 @@
 class DataSource{
-    constructor(onSuccess, onFailed){
-        this.onSuccess = onSuccess;
-        this.onFailed = onFailed;
-    }
 
-    searchClub(keyword){
-        const filteredClubs = clubs.filter(club => {
-            return club.name.toUpperCase().includes(keyword.toUpperCase());
+    // keyword static untuk pengakses method ini, ketika dipanggil dari class, tanpa membuat instance atau pakai keyword new
+    static searchClub(keyword){
+        return new Promise((resolve, reject) => {
+            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+            if(filteredClubs.length){
+                resolve(filteredClubs);
+            }else{
+                reject(`${keyword} is not found`);
+            }
         });
-
-        if(filteredClubs.length){
-            this.onSuccess(filteredClubs);
-        }else{
-            this.onFailed(`${keyword} is not found`);
-        }
     }
 }
