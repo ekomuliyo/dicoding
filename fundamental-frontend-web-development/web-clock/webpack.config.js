8ff75e6f7415174797fd51882019d49d87a97e6f
eko muliyo <ekomuliyo@gmail.com>
2020-04-20 21:51:00 +0700

    plugin webpack

--- fundamental-frontend-web-development/web-clock/webpack.config.js
+++ fundamental-frontend-web-development/web-clock/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -35,5 +36,13 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    // plugin
+    plugins: [
+        // HTML Webpack Plugin
+        new HtmlWebpackPlugin({
+            template: "./src/template.html", // sumber nya
+            filename: "index.html" // tujuan nya di folder /dist
+        })
+    ]
 }
