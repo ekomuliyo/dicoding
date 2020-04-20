1ea5ec7d5857e9b1d40206f51da6a9937aecffb8
eko muliyo <ekomuliyo@gmail.com>
2020-04-20 07:35:18 +0700

    css loader, style loader, babel loader

--- fundamental-frontend-web-development/web-clock/webpack.config.js
+++ fundamental-frontend-web-development/web-clock/webpack.config.js
@@ -6,5 +6,34 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
-    mode: "production"
+    mode: "production",
+    module: {
+        rules: [
+            //  style and css loader
+            {   
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            // babel loader adalah memungkinan kode ES6 dapat berjalan di semua versi browser
+            {
+                test: /\.js$/,
+                exclude: "/node_modules", // mengecualikan seluruh file di folder node_modules, karena kita tidak butuh file2 js di folder ini
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"] // memungkinkan kita menggunakan ES terbaru tanpa mendefinisikan versi spesifik
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 }
