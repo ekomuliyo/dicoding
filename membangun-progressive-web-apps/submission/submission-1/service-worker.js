13279a9323d28424d4e7f96f81826aba6e435aac
eko muliyo <ekomuliyo@gmail.com>
2020-05-15 19:22:55 +0700

    implement all assets to cache and add icon to home screen

--- membangun-progressive-web-apps/submission/submission-1/service-worker.js
+++ membangun-progressive-web-apps/submission/submission-1/service-worker.js
@@ -2,30 +2,75 @@ const CACHE_NAME = "galeriku-v1";
 
 const scripts = [
     "materialize.min.js",
-    "nav-bar.js"
+    "scripts.js"
 ].map(script => `/js/${script}`);
 
 const css = [
-    "materialize.min.css"
-].map(css => `'/css/${css}`);
+    "materialize.min.css",
+    "main.css"
+].map(css => `/css/${css}`);
 
 const imageAssets = [
-    "office.jpg",
-    "pp.png"
+    "pp.png",
+    "icon-512x512.png",
+    "gambar1.jpg",
+    "gambar2.jpg",
+    "gambar3.jpg",
+    "gambar4.jpg",
 ].map(image => `/assets/${image}`);
 
 const urlsToCache = [
     "/",
-    "index.html",
-    "nav-top.html",
-    "nav-side.html",
-    "/galeriku.html",
-    "/login.html",
-    "/register.html",
-    "/tentang.html",
+    "/manifest.json",
+    "/index.html",
+    "/nav-top.html",
+    "/nav-side.html",
+    "pages/beranda.html",
+    "/pages/galeriku.html",
+    "/pages/login.html",
+    "/pages/register.html",
     ...imageAssets,
     ...css,
     ...scripts
 ];
 
 
+// mendaftarkan file di cache
+self.addEventListener("install", function(e){
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            return cache.addAll(urlsToCache); // memasukan semua cache
+        })
+    );
+});  
+
+// handel request, jika request assets ada di dalam cache, maka akan mengambil dari cache
+// namun apabila tidak ada di dalam cache maka request akan diteruskan ke server
+self.addEventListener("fetch", function(e){
+    e.respondWith(
+        caches
+            .match(e.request, { cacheName: CACHE_NAME})
+            .then(function(response){
+                if(response){
+                    return response; // memuat assets dari cache
+                }
+
+                return fetch(e.request); // memuat assets dari server
+            })
+    );  
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+        return Promise.all(
+            cacheNames.map(function(cacheName) {
+            if (cacheName != CACHE_NAME) {
+                console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                return caches.delete(cacheName);
+            }
+            })
+        );
+        })
+    );
+});
